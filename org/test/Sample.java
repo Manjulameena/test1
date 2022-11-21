fcab1c6393dbe93e694f018384b32a8fe7f8d42c
Manjulameena <mailtomanjulamurugan@gmail.com>
2022-11-21 22:14:26 +0530

    code changed

---
+++ org/test/Sample.java
@@ -0,0 +1,13 @@
+package org.test;
+
+public class Sample {
+private void sam() {
+	System.out.println("manju");
+	System.out.println("branch1");
+	System.out.println("branch2");
+}
+public static void main(String[] args) {
+	Sample a = new Sample();
+	a.sam();
+	}
+}
